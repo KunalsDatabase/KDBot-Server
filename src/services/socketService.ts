a2ac09d050ca3391d4457a8c992d16d46d2f0140
Kunal Chopra <kunal@kdbase.com>
2023-10-11 22:42:03 -0400

    socket service

--- src/services/socketService.ts
+++ src/services/socketService.ts
@@ -1,5 +1,22 @@
-const fetchDataFromSockets = (msg:string,host:string,port:number) => {
-    return "test"
-  }
+import { Socket } from 'net'
+
+const fetchDataFromSockets = (msg:string,host:string,port:number): Promise<string> => {
+    return new Promise((resolve, reject) => {
+        const socket = new Socket()
+        socket.connect(port, host, () => {
+            socket.write(msg)
+        })
+        
+        socket.on('data', (data) => {
+            socket.destroy()
+            resolve(data.toString())
+        })
+        
+        socket.on('error', (err) => {
+            reject(err)
+        })
+    })
+}
+
 export default fetchDataFromSockets
   
