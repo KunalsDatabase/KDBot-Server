942395af06e0a77e711fd92b34ce3c220ffceeca
Kunal Chopra <kunal@kdbase.com>
2023-11-06 20:31:33 -0500

    session validation logic

--- src/middlewares/sessionValidation.ts
+++ src/middlewares/sessionValidation.ts
@@ -1,4 +1,16 @@
-import { Request, Response, NextFunction } from 'express'
+import {Request,Response, NextFunction } from 'express'
+import {getSession} from '../services/sessionService'
 export const sessionValidation = (req:Request, res:Response, next:NextFunction) => {
-    
+    const sessionId = req.cookies?.sessionId
+    console.log(req.cookies)
+    if(!sessionId){
+        return res.status(401).json({error:"Unauthorized"})
+    }
+    getSession(sessionId).then((sessionData)=>{
+        if(!sessionData){
+            return res.status(401).json({error:"Unauthorized"})
+        }
+        req.accessToken = sessionData.accessToken
+        next()
+    })
   }
