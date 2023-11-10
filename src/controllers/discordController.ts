693d87541a260f3cc7881ff9b855f50c168cdadb
Kunal Chopra <kunal@kdbase.com>
2023-11-09 22:33:43 -0500

    implementation

--- src/controllers/discordController.ts
+++ src/controllers/discordController.ts
@@ -1,7 +1,19 @@
 import { Request, Response } from 'express'
 import dotenv from 'dotenv'
+import axios from 'axios'
 dotenv.config()
 
 export const getUser =  (req: Request, res: Response) => {
-  return res.status(200)
+  axios.get('https://discord.com/api/oauth2/@me', {
+    headers: {
+      Authorization: `Bearer ${req.accessToken}`
+    }
+  })
+  .then((response)=>{
+    return res.json(response.data.user)
+  })
+  .catch((err)=>{
+    console.log(err)
+    return res.status(500).json({ error: 'Failed to fetch user' })
+  })
 }
