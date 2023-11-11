2047b9fdb77b8466936deeb80ba9dcbbf67c55b5
Kunal Chopra <kunal@kdbase.com>
2023-11-11 13:36:27 -0500

    https server for dev

--- src/index.ts
+++ src/index.ts
@@ -2,23 +2,41 @@ import express from 'express'
 import dotenv from 'dotenv'
 import aggregateRoutes from './routes/aggregateRoutes'
 import oauthRoutes from './routes/oauthRoutes'
+import discordRoutes from './routes/discordRoutes'
 import "reflect-metadata"
 import {AppDataSource} from './database/connection'
 import {initializeCache} from './services/sessionService'
+import https from 'https'
+import fs from 'fs'
+import cookieParser from 'cookie-parser'
 dotenv.config()
-const app = express()
-const PORT = process.env.PORT
+
+let app:express.Express|https.Server = express()
 app.use((req, res, next) => {
   // Set CORS headers
-  res.header('Access-Control-Allow-Origin', '*')
+  res.header('Access-Control-Allow-Origin', 'https://192.168.0.62:3000')
   res.header('Access-Control-Allow-Headers', '*')
+  res.header('Access-Control-Allow-Credentials','true')
   next()
 })
+app.use(cookieParser())
 app.use('/v1', aggregateRoutes)
 app.use('/v1', oauthRoutes)
+app.use('/v1',discordRoutes)
+
+if(process.env.NODE_ENV === 'development'){
+  console.log('Running in development mode')
+  const options = {
+    key: fs.readFileSync(process.env.KEY_PATH as string),
+    cert: fs.readFileSync(process.env.CERT_PATH as string)
+  }
+  app = https.createServer(options, app)
+}
+
+const PORT = process.env.PORT
 
 app.listen(PORT, () => {
-  console.log(`The server is running on http://localhost:${PORT}`)
+  console.log(`The server is running on localhost:${PORT}`)
 })
 
 AppDataSource.initialize().then(() => {
