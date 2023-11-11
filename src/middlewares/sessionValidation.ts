feafbbe2c964b423ca1e045b46652f1bd495a9ec
Kunal Chopra <kunal@kdbase.com>
2023-11-11 13:34:31 -0500

    change to include entire session object

--- src/middlewares/sessionValidation.ts
+++ src/middlewares/sessionValidation.ts
@@ -2,7 +2,6 @@ import {Request,Response, NextFunction } from 'express'
 import {getSession} from '../services/sessionService'
 export const sessionValidation = (req:Request, res:Response, next:NextFunction) => {
     const sessionId = req.cookies?.sessionId
-    console.log(req.cookies)
     if(!sessionId){
         return res.status(401).json({error:"Unauthorized"})
     }
@@ -10,7 +9,7 @@ export const sessionValidation = (req:Request, res:Response, next:NextFunction) 
         if(!sessionData){
             return res.status(401).json({error:"Unauthorized"})
         }
-        req.accessToken = sessionData.accessToken
+        req.session = sessionData
         next()
     })
   }
